2a0428840838929bee7ecc42132d896f88a2c3a9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-20 18:31:33 -0300

    Feat: estrutura da aplicação: cores e fontes

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,21 +17,12 @@ export const GlobalStyle = createGlobalStyle`
 
 
 
-  :focus {
-
-    outline: 0;
-
-    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
-
-  }
-
-
 
   body {
 
-    background-color: ${props => props.theme['gray-800']};
+    background-color: ${props => props.theme['base-background']};
+    color: ${props => props.theme['base-text']};
 
-    color: ${props => props.theme['gray-100']};
 
     -webkit-font-smoothing: antialiased;
 
@@ -41,7 +32,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body, input, textarea, button {
 
-    font: 400 1rem Roboto, sans-serif;
+    font: 400 1rem Nunito, sans-serif;
 
   }
 
