2e40a4576cdfec65748a06abd90850069f149662
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-20 20:38:19 -0300

    Feat: realiza requição para o pefil do gitHub, popula o estado

--- src/context/GitHubContext.tsx
+++ src/context/GitHubContext.tsx
@@ -1,17 +1,53 @@
 import {
     createContext,
     ReactNode,
+    useEffect,
     useState,
 } from 'react'
+import { api } from '../lib/axios';
 
 
+interface Profile {
+  login: string;
+  id: number;
+  node_id: string;
+  avatar_url: string;
+  gravatar_id: string;
+  url: string;
+  html_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  starred_url: string;
+  subscriptions_url: string;
+  organizations_url: string;
+  repos_url: string;
+  events_url: string;
+  received_events_url: string;
+  type: string;
+  site_admin: boolean;
+  name: string;
+  company?: any;
+  blog: string;
+  location?: any;
+  email?: any;
+  hireable: boolean;
+  bio: string;
+  twitter_username?: any;
+  public_repos: number;
+  public_gists: number;
+  followers: number;
+  following: number;
+  created_at: Date;
+  updated_at: Date;
+}
 
 
   
 
   
 interface GitHubContextType {
-
+  profile: Profile[]
 }
   
 export const GitHubContext = createContext({} as GitHubContextType)
@@ -19,19 +55,36 @@ export const GitHubContext = createContext({} as GitHubContextType)
 interface OrderContextProviderProps {
     children: ReactNode
 }
+
+
   
-  export function GitHubContextProvider({ children }: OrderContextProviderProps) {
-   
+export function GitHubContextProvider({ children }: OrderContextProviderProps) {
+   const [profile, setProfile] = useState<Profile[]>([])
   
+   console.log(profile)
+
+    async function fetchProfile (){
+      const response = await api.get('/users/DanielSiilva')
+
+      setProfile(response.data)
+
+    }
+
+
+    useEffect(()=>{
+      fetchProfile()
+    }, [])
+
     
-    return (
-      <GitHubContext.Provider
+    
+  return (
+    <GitHubContext.Provider
         value={{
-         
+         profile
         }}
-      >
+    >
         {children}
-      </GitHubContext.Provider>
-    )
-  }
+    </GitHubContext.Provider>
+  )
+}
   
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-//Url: base  - serve para definirmos o prefixo das nossas url´s
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3333',
+    baseURL: 'https://api.github.com',
 });
