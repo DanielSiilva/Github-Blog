0c778688758cf8af0d83bd17896576ec44f7d9d7
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-02-20 22:16:55 -0300

    Feat: component profile

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,15 +1,66 @@
+
+import { ArrowSquareOut } from "phosphor-react";
+import github from '../../../../assets/github.svg'
+import follower from '../../../../assets/user-group-solid.svg'
+
+
 import { useContext } from "react";
 import { GitHubContext } from "../../../../context/GitHubContext";
-import { ProfileContainer } from "./styles";
+
+import { 
+    ProfileContainer,
+    PhotoUser,
+    Information,
+    Name,
+    Biography,
+    UserInformation
+} from "./styles";
+
 
 
 export function Profile(){
     const {profile} = useContext(GitHubContext)
-    const {avatar_url} = profile
+    
+    const {avatar_url, name, html_url, bio, followers, company, login} = profile
 
     return (
         <ProfileContainer>
-            <img src={avatar_url}/>
+            <PhotoUser
+                src={avatar_url}
+                alt={name}
+            />
+
+            <Information>
+                
+                <Name>
+                    <h2>{name}</h2>
+
+                    <a href={html_url}> 
+                        github
+                        <ArrowSquareOut size={15} weight='bold' />
+                    </a>
+                </Name>
+
+                <Biography>
+                    {bio}
+                </Biography>
+
+                <UserInformation>
+                    <p> 
+                        <img src={github}/>
+                        {login}
+                    </p>
+
+                    <p>
+                        {company} 
+                    </p>
+
+                    <p> 
+                        <img  src={follower}/>
+                        {followers} seguidores
+                    </p>
+                </UserInformation>
+            </Information>
         </ProfileContainer>
     )
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -4,17 +4,126 @@ import styled from "styled-components";
 
 export const ProfileContainer = styled.div`
 
-    border: 1px solid red;
-
     display: flex;
-
+    gap: 1.5rem;
 
     width: 54rem;
     height: 13.25rem;
     margin-top: -70px;
+    padding: 32px 40px;
 
     background: ${props => props.theme['base-profile']};
     box-shadow: 0px 2px 28px rgba(0, 0, 0, 0.2);
     border-radius: 10px;
+
+`
+
+export const PhotoUser = styled.img`
+    width: 148px;
+    height: 148px;
+    left: 40px;
+    top: calc(50% - 148px/2);
+
+    border-radius: 8px;
+
+
+`
+
+
+export const Information = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:0.8rem;
+
+
+    width: 100%;
+
+`
+
+export const Name = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h1{
+        width: 236px;
+        height: 31px;
+
+        font-family: 'Nunito';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 1.5rem;
+        line-height: 130%;
+        
+        color: ${props => props.theme['base-title']};
+    }
+
+
+    a{
+        display: flex;
+        padding: 0px;
+        gap: 8px;
+
+        width: 67px;
+        height: 19px;
+        
+        text-decoration: none;
+        text-transform: uppercase;
+        
+        font-family: 'Nunito';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 0.75rem;
+        line-height: 160%;
+        
+        color: ${props => props.theme['blue']};
+
+    }
+`
+
+
+export const Biography = styled.div`
+    width: 612px;
+    height: 52px;
+    left: 220px;
+    top: 79px;
+
+
+    font-family: 'Nunito';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 160%;
+    
+    color: ${props => props.theme['base-text']};
+
+
+`
+
+
+export const UserInformation = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    
+    p{
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+
+        font-family: 'Nunito';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 160%;
+        
+        color:  ${props => props.theme['base-subtitle']};
+
+        img{
+            width: 18px;
+            height: 18px;
+
+        }
+    }
 
 `
